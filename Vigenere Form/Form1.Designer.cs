780fcb83995928bf30436af26d461f47ffb2a90e
Dovahkid <gainmikey@gmail.com>
2019-02-22 14:19:13 -0500

    C# Vigenere Encryption v1
    Version 1

---
+++ Vigenere Form/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Vigenere_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.radEncrypt = new System.Windows.Forms.RadioButton();
+            this.radDecrypt = new System.Windows.Forms.RadioButton();
+            this.txtPlainText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(249, 65);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(63, 158);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Location = new System.Drawing.Point(6, 16);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(214, 138);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // radEncrypt
+            // 
+            this.radEncrypt.AutoSize = true;
+            this.radEncrypt.Checked = true;
+            this.radEncrypt.Location = new System.Drawing.Point(249, 5);
+            this.radEncrypt.Name = "radEncrypt";
+            this.radEncrypt.Size = new System.Drawing.Size(61, 17);
+            this.radEncrypt.TabIndex = 2;
+            this.radEncrypt.TabStop = true;
+            this.radEncrypt.Text = "Encrypt";
+            this.radEncrypt.UseVisualStyleBackColor = true;
+            this.radEncrypt.CheckedChanged += new System.EventHandler(this.radEncrypt_CheckedChanged);
+            // 
+            // radDecrypt
+            // 
+            this.radDecrypt.AutoSize = true;
+            this.radDecrypt.Location = new System.Drawing.Point(249, 35);
+            this.radDecrypt.Name = "radDecrypt";
+            this.radDecrypt.Size = new System.Drawing.Size(62, 17);
+            this.radDecrypt.TabIndex = 3;
+            this.radDecrypt.Text = "Decrypt";
+            this.radDecrypt.UseVisualStyleBackColor = true;
+            this.radDecrypt.CheckedChanged += new System.EventHandler(this.radDecrypt_CheckedChanged);
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.Location = new System.Drawing.Point(96, 6);
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(147, 20);
+            this.txtPlainText.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter your text:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter your key:";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(96, 32);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(147, 20);
+            this.txtKey.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblOutput);
+            this.groupBox1.Location = new System.Drawing.Point(16, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(226, 163);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 228);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(321, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 250);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPlainText);
+            this.Controls.Add(this.radDecrypt);
+            this.Controls.Add(this.radEncrypt);
+            this.Controls.Add(this.btnSubmit);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Vigener Cipher";
+            this.groupBox1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.RadioButton radEncrypt;
+        private System.Windows.Forms.RadioButton radDecrypt;
+        private System.Windows.Forms.TextBox txtPlainText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+    }
+}
+
---
+++ Vigenere Form/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using vignere_library;
+
+namespace Vigenere_Form
+{
+    public partial class Form1 : Form
+    {
+        bool isEncrypting = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string strPlainText;
+            string strKey;
+
+            try
+            {
+                strPlainText = txtPlainText.Text.ToUpper();
+                strKey = txtKey.Text.ToUpper();
+
+
+                for (int i = 0; i <= strPlainText.Length - 1; i++)
+                {
+                    if (!vig.alphabet.Contains(strPlainText[i].ToString()))
+                    {
+                        strPlainText = strPlainText.Replace(strPlainText[i].ToString(), "");
+                        
+                        
+                    }
+                }
+
+                for (int i = 0; i <= strKey.Length - 1; i++)
+                {
+                    if (!vig.alphabet.Contains(strKey[i].ToString()))
+                    {
+                        strKey = strKey.Replace(strKey[i].ToString(), "");
+                        
+                    }
+                }
+
+                switch (isEncrypting)
+                {
+                    case true:
+
+                        lblOutput.Text = vig.encrypt(strPlainText, strKey);
+                        break;
+                    case false:
+
+                        lblOutput.Text = vig.decrypt(strPlainText, strKey);
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+            catch
+            {
+                lblStatus.Text = "Please enter a valid string.";
+            }
+            
+        }
+
+        private void radEncrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            isEncrypting = true;
+        }
+
+        private void radDecrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            isEncrypting = false;
+        }
+    }
+}
